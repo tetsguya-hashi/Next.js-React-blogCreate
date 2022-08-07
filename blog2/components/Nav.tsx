cac3b82d716c06c584a53c69c23f77690d415d98
tetsuya-hashi <web-partner@tainer-web-partner.com>
2022-08-07 16:54:18 +0900

    ハンバーガーメニューを実装した。

--- blog2/components/Nav.tsx
+++ blog2/components/Nav.tsx
@@ -1,25 +1,42 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Nav.module.scss'
 
 
 const Nav = () => {
+  const [navIsOpen, setNavIsOpen] = useState<boolean>(false);
+  const toggleNav = () => setNavIsOpen(!navIsOpen);
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      {navIsOpen && (
+        <style jsx global>{`
+        @media screen and (max-width: 768px) {
+        body {
+          overflow: hidden;
+          position: fixed;
+          width: 100%;
+        }
+        }
+        `}</style>
+      )}
+      <button className={styles.btn} onClick={toggleNav}>
+        <span className={styles.bar}></span>
+        <span className='sr-only'>MENU</span>
+      </button>
       <ul className={styles.list}>
         <li>
           <Link href='/'>
-            <a>Home</a>
+            <a onClick={toggleNav}>Home</a>
           </Link>
         </li>
         <li>
           <Link href='/about'>
-            <a>About</a>
+            <a onClick={toggleNav}>About</a>
           </Link>
         </li>
         <li>
           <Link href='/blog'>
-            <a>Blog</a>
+            <a onClick={toggleNav}>Blog</a>
           </Link>
         </li>
       </ul>
